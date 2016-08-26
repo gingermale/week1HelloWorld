f541c322cfbbb41a68df27d916f3abc13f1294b2
Reese Focazio-Moran <Parzival@Reeses-MacBook-Pro.local>
2016-08-25 21:11:47 -0700

    Adding my source code

---
+++ src/HelloWorld_rfocaziomoran.java
@@ -0,0 +1,15 @@
+
+//Reese Focazio-Moran
+//AP Computer Science
+//August 8, 2016
+//Mr. Balanda
+//Weekly Project 1: HelloWorld
+public class HelloWorld_rfocaziomoran {
+	public static void main(String args[]) {
+	//This is the "main" routine
+System.out.println("Hello World");
+//That line outputs the greeting
+System.out.println("Reese Focazio-Moran");
+//That line outputs my name
+}
+}
