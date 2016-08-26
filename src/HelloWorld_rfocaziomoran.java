3e4765c3eaa3385379d39e22ae57c3e2cd7bcc57
Reese Focazio-Moran <reese@focazio-moran.com>
2016-08-25 21:16:13 -0700

    Let's reorganize a little

--- src/HelloWorld_rfocaziomoran.java
+++ src/HelloWorld_rfocaziomoran.java
@@ -1,15 +1,17 @@
+// Name: Reese Focazio-Moran
+// Course: AP Computer Science
+// Date: August 8, 2016
+// Teacher: Mr. Balanda
+// Weekly Project 1: HelloWorld
 
-//Reese Focazio-Moran
-//AP Computer Science
-//August 8, 2016
-//Mr. Balanda
-//Weekly Project 1: HelloWorld
 public class HelloWorld_rfocaziomoran {
 	public static void main(String args[]) {
-	//This is the "main" routine
-System.out.println("Hello World");
-//That line outputs the greeting
-System.out.println("Reese Focazio-Moran");
-//That line outputs my name
-}
+		// This is the "main" routine
+		System.out.println("Hello World");
+
+		// That line outputs the greeting
+		System.out.println("Reese Focazio-Moran");
+		
+		// That line outputs my name
+	}
 }
